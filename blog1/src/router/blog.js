b8fa8c6b778c782946aed7d1903304c07069a998
ypinchina <batmanyp@foxmail.com>
2020-08-26 20:18:25 +0800

    '完成所有前端功能'

--- blog1/src/router/blog.js
+++ blog1/src/router/blog.js
@@ -2,6 +2,7 @@ const { getList, getDetail, newBlog, updateBlog, deleteBlog } = require('../cont
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 const handleBlogRouter = (req, res) => {
+    let id = req.query.id
     // 处理blog路由的接口
     const loginCheck = () => {
         //判断是否用户是否有登录的方法
@@ -17,6 +18,16 @@ const handleBlogRouter = (req, res) => {
     if(method === 'GET' && req.path === '/api/blog/list') {
         let author = req.query.author || ''
         let keyword = req.query.keyword || ''
+        let isAdmin = req.query.isAdmin || ''
+        if(isAdmin) {
+            let loginCheckResult = loginCheck(req)
+            if (loginCheckResult) {
+                //有结果 没有登录
+                return loginCheckResult/** 这句话相当于返回这个 return Promise.resolve(
+                                                new ErrorModel('尚未登录')) 不会往下执行了 */
+            }
+            author = req.session.username
+        }
         let result = getList(author, keyword)
         return result.then(listData => {
             return new SuccessModel(listData)
@@ -24,7 +35,6 @@ const handleBlogRouter = (req, res) => {
     }
     //获取博客详情
     if(method === 'GET' && req.path === '/api/blog/detail') {
-        let id = req.query.id
         let detailResult = getDetail(id)
         return detailResult.then(data => {
             return new SuccessModel(data)
@@ -53,7 +63,7 @@ const handleBlogRouter = (req, res) => {
             return loginCheckResult/** 这句话相当于返回这个 return Promise.resolve(
                                             new ErrorModel('尚未登录')) 不会往下执行了 */
         }
-        let updateResult = updateBlog(req.query.id, req.body) //返回的result是个boolean
+        let updateResult = updateBlog(id, req.body) //返回的result是个boolean
         return updateResult.then(result => {
             if(result) {
                 return new SuccessModel()
@@ -70,7 +80,7 @@ const handleBlogRouter = (req, res) => {
             return loginCheckResult/** 这句话相当于返回这个 return Promise.resolve(
                                             new ErrorModel('尚未登录')) 不会往下执行了 */
         }
-        let deleteResult = deleteBlog(req.query.id, 'lisi')
+        let deleteResult = deleteBlog(id, req.session.username)
         return deleteResult.then(result => {
             if(result) {
                 return new SuccessModel()
