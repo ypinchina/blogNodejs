948ccda305a60c207fadd201476f1ba1c7d632d3
ypinchina <batmanyp@foxmail.com>
2020-08-28 07:01:58 +0800

    '密码加密'

--- blog1/src/controller/user.js
+++ blog1/src/controller/user.js
@@ -1,10 +1,11 @@
 const { exec, escape } = require('../database/db')
+const xss = require('xss')
+const genPassword = require('../utils/cryp')
 const login = (username, password) => {
     username = escape(xss(username))
-    password = escape(xss(password))
+    password = escape(genPassword(xss(password)))
     const sql = `select username, password from 
     users where username = ${ username } and password = ${ password };`
-    console.log('sql is', sql)
     return exec(sql).then(res => {
         return res[0]
     })
